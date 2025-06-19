0799dfa6ea2eddffe3ff5ee7e2dbf061bf389c27
Maoulid94 <soubaneh2015@outlook.fr>
2025-06-19 13:53:33 +0300

    fix: refactor header component for improved structure and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,13 @@ export const Header = () => {
     <div className="header">
       <img className="drapeau" src="/drapeau.png" alt="logo" />
       <h2>
-        Laboratoire de Recherche en Intelligence Artificielle (
+        <span className="hidden">
+          Laboratoire de Recherche en Intelligence Artificielle (
+        </span>
         <span className="la">La</span>
         <span className="r">R</span>
-        <span className="ai">AI</span>)
+        <span className="ai">AI</span>
+        <span className="hidden">)</span>
       </h2>
       <img className="logo" src="/logo.png" alt="logo" />
     </div>
