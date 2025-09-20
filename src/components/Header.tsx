839e710b95994c67307f0e1f128f5da9c9677455
Maoulid94 <soubaneh2015@outlook.fr>
2025-09-20 17:31:06 +0300

    last update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,6 @@ export const Header = () => {
       <img className="ansie" src="/icon.png" alt="logo" />
       <h2>
         <span className="hidden">Text Analysis</span>
-        {/* <span className="la">La</span>
-        <span className="r">R</span>
-        <span className="ai">IA</span>
-        <span className="hidden">)</span> */}
       </h2>
       <img className="logo" src="/background.jpg" alt="logo" />
     </div>
